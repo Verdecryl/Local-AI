f2632db5ae9c93b4e4ce1a0c9834cd3e478afabc
Verdecryl <adrian.plesca.2006@gmail.com>
2026-02-17 22:14:53 +0200

    Refactor file paths and API key in ask-ai.py
    Updated file paths and API key placeholder in ask-ai.py.

--- ask-ai.py
+++ ask-ai.py
@@ -16,22 +16,18 @@ def read_file(filename):
     except OSError as e:
         print(f"Error reading file '{filename}': {e}")
         return None
+#Path to in and out files
+inn="Your Path To question.txt"
+out="Your Path To answer.txt"
 
-inn="C:/Users/Adrian/Desktop/question.txt"
-out="C:/Users/Adrian/Desktop/answer.txt"
-
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-#1: AIzaSyA-DZIAHeiVFsTGbbycRAhBoAsJm0mceBY
-#2: AIzaSyBDNODMwvvcVpue2mM6mObbLxoAcyM89M8
-#3: AIzaSyCjJasTN28oiJrYVF7WCz3-JPFsNKzsI38
-#4: AIzaSyC74HEVtJ_EA4zugwUs-LI06RUQrDLJ9tk
-#5: AIzaSyArISjICT8UPQjtTnI9sGbzOVl3orOcw8M
-GEMINI_API_KEY = "AIzaSyA-DZIAHeiVFsTGbbycRAhBoAsJm0mceBY"
+#Here goes the GEMINI API KEY
+GEMINI_API_KEY = "Your-API-KEY"
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents=read_file(inn)
 )
+
 
 write_file(out, response.text)
